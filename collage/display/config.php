b3085c22ea8ba8a10b1b16d46406fdb8d57f0648
Manan Tayal <robotics.tayalmanan@gmail.com>
2022-03-13 18:41:01 +0530

    Website v1.0

---
+++ collage/display/config.php
@@ -0,0 +1,38 @@
+<?php 
+
+$images_details = array(
+[ 'photo'=> 'resources/images/faculty_speakers/chaya_csa.jpeg' ,
+  'webpage' => 'https://www.csa.iisc.ac.in/~chaya/'],
+
+[ 'photo'=> 'resources/images/faculty_speakers/phanim_cds.jpeg',
+  'webpage' => 'https://cds.iisc.ac.in/people/faculty/name/phani-motamarri/'],
+
+[ 'photo'=> 'resources/images/faculty_speakers/vishnu_ee.jpeg',
+  'webpage' => 'http://www.ee.iisc.ac.in/faculty/vishnu/index.php'],
+
+[ 'photo'=> 'resources/images/faculty_speakers/sundeep_ece.jpeg',
+'webpage' => 'https://ece.iisc.ac.in/~spchepuri/'],
+
+[ 'photo'=> 'resources/images/faculty_speakers/jishnu_rbccps.jpeg',
+'webpage' => 'https://mecheng.iisc.ac.in/people/jishnu-keshavan/'],
+
+[ 'photo'=> 'resources/images/faculty_speakers/arup_ese.jpg',
+'webpage' => 'talk.php?id=5'],
+
+// [ 'photo'=> 'resources/images/plenary_speakers/lalitesh.jpg', 
+//  'webpage' => 'talk.php?id=7'],
+
+// [ 'photo'=> 'resources/images/plenary_speakers/malik.gif',
+//  'webpage' => 'talk.php?id=8'],
+
+//  [ 'photo'=> 'resources/images/plenary_speakers/tambe.jpg',
+//  'webpage' => 'talk.php?id=9'],
+
+);
+
+// $images = array(
+// 'resources/images/faculty_speakers/chaya_csa.jpeg',
+// 'resources/images/faculty_speakers/phanim_cds.jpeg',
+// 'https://eecs.iisc.ac.in/EECS2020/details/photos/1._Y_Narahari.jpg',
+// );
+
